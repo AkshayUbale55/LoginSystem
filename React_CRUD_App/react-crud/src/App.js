2418f7c5b33ce8f7e1d700ccd0d09260fec722d3
PranxXxD <saipranayalishetty@gmail.com>
2024-04-08 17:31:40 +0530

    Update App.js

--- React_CRUD_App/react-crud/src/App.js
+++ React_CRUD_App/react-crud/src/App.js
@@ -1,80 +1,50 @@
-import { useEffect, useState } from "react"
-import { TaskList } from "./component/TaskList";
-import Api from "./Api";
 
+import { BrowserRouter as Router, Routes, Route  } from "react-router-dom";
+import {Home} from "./pages/Home";
+import {Contact} from "./pages/Contact";
+import { Navbar } from "./component/Navbar";
+import { Profile } from "./pages/Profile";
+import { createContext, useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Catfact } from "./pages/Catfact";
+import { Provider } from "react-redux";
+import { store } from "./component/store";
+import { Login } from "./pages/Login";
+import { CreatePost } from "./pages/post/create-post";
+import { MsdLink } from "./pages/msdLink";
+
+export const  AppContext =  createContext()
 
 function App() {
-
-  const [todoList, setTodoList] = useState([]);
-  const [newTask, setTask] = useState("");
-
-  useEffect(() => {
-    console.log("Component Mounted");
-
-    return () => {
-      console.log("Component Unmounted")
+  const [userName, setUserName] = useState("saipranay")
+  const client = new QueryClient({defaultOptions: {
+    // prevent the updation of data in frontend when the user switches the tab
+    queries: {
+      refetchOnWindowFocus :false,
     }
-  }, [])
-
-  const handleChange = (event) => {
-    setTask(event.target.value)
-  }
-
-  const addtask = () => {
-    // const newTodoList = [...todoList, task]
-    const task = {
-      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
-      taskName: newTask,
-      completed: false,
-    }
-    setTodoList([...todoList, task]);
-    console.log(todoList)
-  }
-
-
-  const deleteTask = (id) => {
-    // filtering the old todo list with the selected task name and returning the new list
-    setTodoList(todoList.filter((task) => {
-      return task.id !== id
-    }))
-  }
-
-  const updateTask = (id) => {
-    // update the old todo list with the selected task name and returning the new list
-    setTodoList(todoList.map((task) => {
-      if (task.id === id) {
-        console.log("updated");
-        return { ...task, completed: true };
-      }
-      else {
-        console.log("Not updated");
-        return task;
-      }
-    }))
-  }
-
-
+  }})
   return (
     <div className="App">
-      <div className="mt-2">
-        <input
-          onChange={handleChange}
-          className="flex m-auto w-80 mt-9 rounded-md border-0 py-1.5 p-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-purple-600 sm:text-sm sm:leading-6"
-        />
-        <button
-          onClick={addtask}
-          className="flex m-auto w-64 my-6 justify-center rounded-md bg-purple-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-voilet-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-deep-purple-500"
-        >
-          Add Task
-        </button>
-      </div>
-      <div className="bg-blue-gray-100  m-auto w-[30rem]">
-        {todoList.map((task) => {
-          return (
-            <TaskList taskName={task.taskName} id={task.id} deleteTask={deleteTask} completed={task.completed} updateTask={updateTask} />)
-        })}
-      </div>
-      <Api/>
+    <QueryClientProvider client={client}>
+    <AppContext.Provider value={{userName,setUserName}}>
+    <Provider store={store}>
+    <Router>
+    <Navbar/>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/profile" element={<Profile />}/>
+      <Route path="/contact" element={<Contact/>}/>
+      <Route path="/catfact" element={<Catfact/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/createpost" element={<CreatePost/>}/>
+      <Route path="/msdlink" element={<MsdLink/>}/>
+      <Route path="*" element={<h1 className="mt-4 font-bold text-3xl text-center p-4 text-deep-purple-600">Page is not found 404</h1>}/>
+      
+    </Routes>
+    </Router>
+    </Provider>
+    </AppContext.Provider>
+    </QueryClientProvider>
     </div>
   );
 }
