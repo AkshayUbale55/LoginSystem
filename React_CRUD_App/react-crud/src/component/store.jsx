1b5634b2fb6440696127b54ee7862da538b9c327
PranxXxD <saipranayalishetty@gmail.com>
2024-04-12 18:54:27 +0530

    Create store.jsx

---
+++ React_CRUD_App/react-crud/src/component/store.jsx
@@ -0,0 +1,23 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+const  userSlice = createSlice({
+    name: 'user',
+    initialState: {value : {userName : ""}},
+    reducers : {
+        login: (state,action)=>{
+            state.value = action.payload;
+        },
+        logout :(state)=>{
+            state.value = {value : {userName : ""}}
+        }
+    }
+})
+
+export const  {login,logout} = userSlice.actions;
+
+
+export const store = configureStore({
+    reducer: {
+        user : userSlice.reducer,
+    }
+})
